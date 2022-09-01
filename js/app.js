2da0a7bc00fd9a4e311903a7b545464e53b37f14
harsh <109368583+harshsharma1981@users.noreply.github.com>
2022-09-02 00:30:29 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,25 @@
 
 
 
+
 showdiary()
 let addbtn = document.getElementById("addbtn")
 addbtn.addEventListener("click", function(e){
-    let addtxt = document.getElementById("addtxt")
-    let diary = localStorage.getItem("diary")
+    let addtxt = document.getElementById("addtxt");
+    let title = document.getElementById("title");
+    let date1 = document.getElementById("date1");
+    let diary = localStorage.getItem("diary");
+  
     if (diary == null) {
         diaryobj = [];
     }
     else{
         diaryobj = JSON.parse(diary)
     }
-    diaryobj.push(addtxt.value);
+    diaryobj.push(title.value + date1.value + addtxt.value );
     localStorage.setItem("diary", JSON.stringify(diaryobj))
     addtxt.value = ""
-    console.log(diaryobj)
+    console.log(date1.value)
     showdiary()
 })
 
@@ -27,14 +31,30 @@ function showdiary() {
     else{
         diaryobj = JSON.parse(diary);
     }
+    let t2 = document.getElementById("title")
     let html = "";
     diaryobj.forEach(function(element, index) {
+        let elee = element.substring(0, 10);
+        let eleee = elee.replace(/[0-9]/g, '');
+         let ele = eleee.replace(/[-]/g, '');
+
+      let ele33 = element.slice(0, 16)
+      let ele3 = ele33.replace(/[A-Za-z]/g, "") 
+      let ele222 = element.replace(`${ele3}`, '');
+    let ele22 = ele222.replace(`${ele}`, '');
+      let ele2 = ele22.slice(0, 3000000);
+    //   let ele333 = ele22.slice(0, 10);
+
+     
+
         html += `
         <div class="diarycard my-2 mx-2" style="width: 23rem;">
         <div class="card-body">
-          <h5 class="card-title">my diary ${index + 1}</h5>
+          <h5 class="card-title" maxlength="10">${ele}</h5>
           <hr>
-          <p id="coll"  class="card-text " >${element}</p>
+          <h5 class="card-date" >${ele3}</h5>
+          <hr>
+          <p id="coll"  class="card-text " >${ele2}</p>
           <button id="${index}" onclick="deletediary(this.id)" class="btn btn-primary">Delete</button>
         </div>
       </div>
